66793898020429428c58d5b0550d35f9273fa679
Ayujijis <pichuk99@gmail.com>
2025-06-16 15:56:29 +0200

    Delete Gamepython copy directory

--- Gamepython copy/organisms/animal/Sheep.py
+++
@@ -1,6 +0,0 @@
-from organisms.Animal import Animal
-
-class Sheep(Animal):
-    IMAGE_FILE = "images/sheep.png"
-    def __init__(self, world, position):
-        super().__init__(world, position, strength=4, initiative=4, color="white")
--- Gamepython copy/organisms/plants/Grass.py
+++
@@ -1,6 +0,0 @@
-from organisms.Plant import Plant
-
-class Grass(Plant):
-    IMAGE_FILE = "images/grass.png"
-    def __init__(self, world, position):
-        super().__init__(world, position, strength=0, initiative=0, color="lightgreen")
